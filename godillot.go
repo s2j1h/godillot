579e274ce186b810bd6c23d0735b1a14d16c7060
julien <julien@zeneffy.fr>
2016-03-28 23:27:59 +0200

    added html output

--- godillot.go
+++ godillot.go
@@ -5,13 +5,16 @@ import (
 	"fmt"
 	"golang.org/x/net/html/charset"
 	"gopkg.in/yaml.v2"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
-type Monit struct {
+//Structures for monit xml files
+type Server struct {
+	Name     string
 	Services []Service `xml:"service"`
 }
 
@@ -30,7 +33,7 @@ type Stat struct {
 }
 
 func (s Service) String() string {
-	return fmt.Sprintf("%s\tStatus: %d\tUptime: %s\tMemory: %.1f%%\tCPU: %.1f%%", s.Name, s.Status, transformUptime(s.Uptime), s.Memory.Percent, s.Memory.Percent)
+	return fmt.Sprintf("%s\tStatus: %d\tUptime: %s\tMemory: %.1f%%\tCPU: %.1f%%", s.Name, s.Status, transformUptime(s.Uptime), s.Memory.Percent, s.CPU.Percent)
 }
 
 //Transform uptime (seconds) to string "dayshoursminutes"
@@ -41,11 +44,13 @@ func transformUptime(uptime int32) string {
 	return fmt.Sprintf("%dd%dh%dm", days, hours, minutes)
 }
 
+//Struct file for configuration file
 type Conf struct {
-	Servers []Server `yaml:"servers"`
+	Servers    []ServerConf `yaml:"servers"`
+	OutputFile string       `yaml:"outputfile"`
 }
 
-type Server struct {
+type ServerConf struct {
 	Url        string `yaml:"url"`
 	ServerName string `yaml:"server"`
 }
@@ -59,39 +64,75 @@ func (c *Conf) getConf() *Conf {
 	}
 	err = yaml.Unmarshal(yamlFile, c)
 	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
+		log.Fatalf("yamlFile.Unmarshal: %v", err)
 	}
 
 	return c
 }
 
+// Struct for HTML page
+
+type Data struct {
+	Servers []Server
+}
+
+// create html page using go templates
+func createPage(data Data) {
+	t := template.New("server template")
+
+	t = template.Must(t.ParseFiles("layout.html", "server.html"))
+
+	// Exécution de la fusion et injection dans le flux de sortie
+	// La variable p sera réprésentée par le "." dans le layout
+	// Exemple {{.}} == p
+	err := t.ExecuteTemplate(os.Stdout, "layout", data)
+
+	if err != nil {
+		log.Fatalf("createPage.Template: %s", err)
+	}
+}
+
 func main() {
 
 	var configuration Conf
+	var serversList []Server
 	configuration.getConf()
 
-	for _, server := range configuration.Servers {
-		response, err := http.Get(server.Url)
+	fmt.Printf("\n** Godillot v0.5**\n")
+
+	for _, serverConf := range configuration.Servers {
+		response, err := http.Get(serverConf.Url)
 		if err != nil {
-			log.Fatalf("main GetUrl: %v", err)
+			log.Fatalf("main.GetUrl: %v", err)
 			os.Exit(1)
 		}
 		defer response.Body.Close()
 		if err != nil {
-			log.Fatalf("main GetUrl: %v", err)
+			log.Fatalf("main.Body: %v", err)
 			os.Exit(1)
 		}
 
-		var monit Monit
+		var server Server
 		decoder := xml.NewDecoder(response.Body)
 		decoder.CharsetReader = charset.NewReaderLabel
-		err = decoder.Decode(&monit)
-
-		fmt.Printf("\n** %s **\n", server.ServerName)
-		for _, service := range monit.Services {
-			fmt.Printf("%s\n", service)
+		err = decoder.Decode(&server)
+		if err != nil {
+			log.Fatalf("main.Unmarshal: %v", err)
+			os.Exit(1)
 		}
+		server.Name = serverConf.ServerName
+
+		serversList = append(serversList, server)
 
 	}
+
+	for _, server := range serversList {
+		fmt.Printf("\n** %s **\n", server.Name)
+		for _, service := range server.Services {
+			fmt.Printf("%s\n", service)
+		}
+	}
+	htmlData := Data{serversList}
+	createPage(htmlData)
 
 }
